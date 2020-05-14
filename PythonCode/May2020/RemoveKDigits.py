2c70303f5c0341a99fffb296ef7d32904dfdd790
Vixus <vixus7@gmail.com>
2020-05-14 00:48:22 -0400

    New solution to RemoveKDigits

--- PythonCode/May2020/RemoveKDigits.py
+++ PythonCode/May2020/RemoveKDigits.py
@@ -1,32 +1,48 @@
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
-        strNum = ''
+        # strNum = ''
 
-        def findSmallestNum(num, k, strNum):
+        # def findSmallestNum(num, k, strNum):
 
-            if k == 0:
-                return strNum + num
-            elif len(num) == k:
-                if strNum == '':
-                    return 0
-                else:
-                    return strNum
+        #     if k == 0:
+        #         return strNum + num
+        #     elif len(num) == k:
+        #         if strNum == '':
+        #             return 0
+        #         else:
+        #             return strNum
+        #     else:
+        #         smallestNum = min(num[0:k+1])
+        #         smallestIndex = num[0:k+1].index(smallestNum)
+        #         remaingNum = num[smallestIndex+1:]
+        #         numToRemove = k-smallestIndex
+        #         strNum = strNum + smallestNum
+        #         strNum = findSmallestNum(remaingNum, numToRemove, strNum)
+
+        #     return strNum
+
+        # return str(int(findSmallestNum(num,k,strNum)))
+        strNum = '0'
+        totalDigits = len(num)-k
+
+        while k != 0:
+            if k == len(num):
+                num = ''
+                break
+            smallestNum = min(num[0:k+1])
+            smallestIndex = num[0:k+1].index(smallestNum)
+            k = k-(smallestIndex)
+            strNum = strNum + smallestNum
+            if smallestIndex == len(num)-1:
+                num = ''
             else:
-                smallestNum = min(num[0:k+1])
-                smallestIndex = num[0:k+1].index(smallestNum)
-                remaingNum = num[smallestIndex+1:]
-                numToRemove = k-smallestIndex
-                strNum = strNum + smallestNum
-                strNum = findSmallestNum(remaingNum, numToRemove, strNum)
+                num = num[smallestIndex+1:]
 
-            return strNum
-
-        return str(int(findSmallestNum(num,k,strNum)))
-
+        return str(int(strNum+num))
 
 def main():
     s = Solution()
-    ans = s.removeKdigits('10', 2)
+    ans = s.removeKdigits('10',2)
     print(ans)
 
 
--- PythonCode/May2020/test.py
+++ PythonCode/May2020/test.py
@@ -5,3 +5,10 @@ print(a+b)
 print(c.index(min(c)))
 print(c[1:])
 print(int(10))
+
+d = [1,2,3,4]
+print(d[3]) 
+
+for x in range(0):
+    print("hello") 
+    print(range(0))

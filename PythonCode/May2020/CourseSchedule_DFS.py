2d0ef89f30a7f88e1ba801aa29b859ba6ea8eb72
Vixus <vixus7@gmail.com>
2020-05-30 15:07:39 -0400

    Added documentation

--- PythonCode/May2020/CourseSchedule_DFS.py
+++ PythonCode/May2020/CourseSchedule_DFS.py
@@ -4,6 +4,18 @@ from collections import defaultdict
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        """
+        There are a total of numCourses courses you have to take, labeled from 0 to numCourses-1.
+        Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+        Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+
+        Arguments:
+            numCourses {int} -- [description]
+            prerequisites {List[List[int]]} -- [description]
+
+        Returns:
+            bool -- [description]
+        """
         adjDict = defaultdict(list)
         nodeStatus = defaultdict(int)
 

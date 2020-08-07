d50cee5202e2c747cfa1be2c245be17349156f7d
Vixus <vixus7@gmail.com>
2020-08-06 23:02:51 -0400

    Submitting extra comments

--- PythonCode/Monthly_Coding_Challenge/Aug2020/Find_All_Duplicates_in_an_Array.py
+++ PythonCode/Monthly_Coding_Challenge/Aug2020/Find_All_Duplicates_in_an_Array.py
@@ -27,6 +27,7 @@ class Solution:
                 i += 1
             else:
                 nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+                # nums[i], nums[nums[i]-1] = nums[nums[i]-1], nums[i] # THIS IS NO GOOD. ORDER MATTERS
 
         return [nums[x] for x in range(len(nums)) if nums[x] != x+1]
 

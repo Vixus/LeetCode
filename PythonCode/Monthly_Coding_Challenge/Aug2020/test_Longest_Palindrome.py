9432ff3eab5ddc5dc972ab59f6137be646e595ef
Vixus <vixus7@gmail.com>
2020-08-14 09:15:27 -0400

    Submit solution

---
+++ PythonCode/Monthly_Coding_Challenge/Aug2020/test_Longest_Palindrome.py
@@ -0,0 +1,22 @@
+from Longest_Palindrome import Solution
+
+
+def test1():
+    s = Solution()
+    inputStr = 'abccccdd'
+    ans = s.longestPalindrome(inputStr)
+    assert ans == 7
+
+
+def test2():
+    s = Solution()
+    inputStr = 'aaabbbccccddddd'
+    ans = s.longestPalindrome(inputStr)
+    assert ans == 13
+
+
+def test3():
+    s = Solution()
+    inputStr = 'civilwartestingwhetherthatnaptionoranynartionsoconceivedandsodedicatedcanlongendureWeareqmetonagreatbattlefiemldoftzhatwarWehavecometodedicpateaportionofthatfieldasafinalrestingplaceforthosewhoheregavetheirlivesthatthatnationmightliveItisaltogetherfangandproperthatweshoulddothisButinalargersensewecannotdedicatewecannotconsecratewecannothallowthisgroundThebravelmenlivinganddeadwhostruggledherehaveconsecrateditfaraboveourpoorponwertoaddordetractTgheworldadswfilllittlenotlenorlongrememberwhatwesayherebutitcanneverforgetwhattheydidhereItisforusthelivingrathertobededicatedheretotheulnfinishedworkwhichtheywhofoughtherehavethusfarsonoblyadvancedItisratherforustobeherededicatedtothegreattdafskremainingbeforeusthatfromthesehonoreddeadwetakeincreaseddevotiontothatcauseforwhichtheygavethelastpfullmeasureofdevotionthatweherehighlyresolvethatthesedeadshallnothavediedinvainthatthisnationunsderGodshallhaveanewbirthoffreedomandthatgovernmentofthepeoplebythepeopleforthepeopleshallnotperishfromtheearth'
+    ans = s.longestPalindrome(inputStr)
+    assert ans == 983
